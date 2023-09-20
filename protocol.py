6528635cba648510200fbb9260ed9aefd6f16713
tgal0007 <tgal0007@student.monash.edu>
2023-09-20 19:01:41 +1000

    Floats vars & interrupts good, strings need fixing

--- protocol.py
+++ protocol.py
@@ -15,11 +15,18 @@ DESTINATION = "172.20.10.13"  # TODO: this need to change for each user
 should_stop = mp.Event()
 
 # Initialising global dummy variables for now (change later)
-global_x = mp.Value("f", 0.0) # probably should be initialised to a purposeful value in loading zone
-global_y = mp.Value("f", 0.0) # probably should be initialised to a purposeful value in loading zone
-global_theta = mp.Value("f", 90.0) # could be 0 to start off with
-global_mode = mp.Value("c", b'L') # either L or G. start in loading zone
-global_lane = mp.Value("c", b' ') # either A B or C
+# self global variables
+our_x = mp.Value("f", 0.0) # probably should be initialised to a purposeful value in loading zone
+our_y = mp.Value("f", 0.0) # probably should be initialised to a purposeful value in loading zone
+our_theta = mp.Value("f", 90.0) # could be 0 to start off with
+our_mode = mp.Value("c", b'L') # either L or G. start in loading zone
+our_lane = mp.Value("c", b' ') # either A B or C
+# other robot global variables
+their_x = mp.Value("f", 0.0) 
+their_y = mp.Value("f", 0.0)
+their_theta = mp.Value("f", 90.0)
+their_mode = mp.Value("c", b'L')
+their_lane = mp.Value("c", b' ')
 
 def p_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,16 +36,27 @@ def p_server():
 
     client, address = server_socket.accept()
     print(f"connected to {address} on port {PORT}")
-    while True:
+    while not should_stop.is_set():
         data = client.recv(1024)
         decoded_data = data.decode("utf-8")
         variables = decoded_data.split(',')
-        x = variables[0]
-        y = variables[1]
-        theta = variables[2]
-        mode = variables[3]
-        lane = variables[4]
-        print(f"{x},{y},{theta},{mode},{lane}")
+
+        if variables[0].strip():  # Check if the first element is not an empty string
+            try:
+                their_x.value = float(variables[0].strip())
+                their_y.value = float(variables[1].strip())
+                their_theta.value = float(variables[2].strip())
+                
+                # TODO: fix the b'G' format issue in mode and lane variables to just print out a 'G'. Not sure if issue with how i'm initialising the character
+                print(f"Mode = {variables[3]}, Encoded mode = {variables[3].encode('utf-8')[0]}")
+                their_mode.value = variables[3].encode("utf-8")[0]
+                their_lane.value = variables[4].encode("utf-8")[0]
+                print(f"Their values = {their_x.value},{their_y.value},{their_theta.value},{their_mode.value},{their_lane.value}")
+            except ValueError:
+                print("Error: Could not convert the string to a float.")
+        else:
+            print("Error: Empty string received.")
+    # return their_x, their_y, their_theta, their_mode, their_lane
 
 def p_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -56,11 +74,9 @@ def p_client():
             print(e)  # print the error
             sleep(1)  # retry connection in 1 second
 
-    while True:
+    while not should_stop.is_set():
         try:
-            if should_stop.is_set():
-                break # Exit the loop if keyboard interrupt occurs
-            text = f'{global_x.value},{global_y.value},{global_theta.value},{global_mode.value},{global_lane.value}'
+            text = f'{our_x.value},{our_y.value},{our_theta.value},{our_mode.value},{our_lane.value}'
             client_socket.send(bytes(text, "UTF-8"))
             sleep(DELAY)
         except Exception as e:
@@ -78,13 +94,14 @@ if __name__ == "__main__":
         while not should_stop.is_set():
             # Dummy global variables to test this function
             for i in range(50):
-                # print(f"{global_x.value},{global_y.value},{global_theta.value},{global_mode.value},{global_lane.value}") #testing loop works properly
-                global_x.value = i
-                global_y.value = i+1
-                global_theta.value = i+2
-                global_mode.value = b'G'
-                global_lane.value = b'B'
+                print(f"Our values = {our_x.value},{our_y.value},{our_theta.value},{our_mode.value},{our_lane.value}") # testing the values are right
+                our_x.value = i
+                our_y.value = i+1
+                our_theta.value = i+2
+                our_mode.value = b'G'
+                our_lane.value = b'B'
                 time.sleep(DELAY) # simulating delay between updates
     except KeyboardInterrupt:
+        print('Keyboard interrupt detected.')
         # Set the event to signal all threads to stop
         should_stop.set()
